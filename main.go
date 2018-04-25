ba097e6662d5a6d623bffa98f584295f57fdc25f
gorozhin <m.gorozhin@gmail.com>
2018-04-25 14:15:47 +0000

    hf

--- main.go
+++ main.go
@@ -1,43 +1,19 @@
 package main
 
 import (
-	"net"
-	"strconv"
-
-	
-	"./util"
-	"./stockingConnection"
+	"./server"
+	"./auth"
 )
 
-type StockingServer struct{
-	host string
-	port int64
-	listener *net.TCPListener
-}
-
-func (s *StockingServer)Run(){
-	service := s.host+":"+strconv.FormatInt(s.port, 10)
-	
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	util.CheckError(err)
-
-	if (s.listener == nil){
-		s.listener, err = net.ListenTCP("tcp", tcpAddr)
-	}
-	util.CheckError(err)
-
-	for {
-		conn, err := s.listener.Accept()
-		if err != nil {
-			continue
-		}
-		var sc stockingConnection.StockingConnection = stockingConnection.StockingConnection{Conn : conn}
-		go sc.HandleRequest()
-	}
-}
-
 func main() {
-	server := StockingServer{"0.0.0.0", 7777, nil}
+	
+	server := server.StockingServer{
+		7777,
+		nil,
+		auth.Container(map[string]string{
+			"john" : "doe",
+			"login" : "password",
+		})}
 	server.Run()
 }
 
--- util/main.go
+++ util/main.go
@@ -5,6 +5,8 @@ import (
 	"net"
 	"os"
 	"io"
+	"strconv"
+	"strings"
 	
 )
 
@@ -31,4 +33,39 @@ func CheckError(err error) {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 	}
+}
+
+
+func DispIp4(octets []byte) string {
+	str := make([]string, len(octets))
+	for i := 0; i < len(str); i++ {
+		str[i] = strconv.FormatInt(int64(octets[i]), 10)
+	}
+	return strings.Join(str, ".")
+
+}
+
+func DispDomain(octets []byte) string {
+	return string(octets[:len(octets)])
+}
+
+func DispIp6(octets []byte) string {
+
+	str := make([]string, len(octets))
+	for i := 0; i < len(str); i++ {
+		pretender_str := strconv.FormatInt(int64(octets[i]), 16)
+		pretender_str_len := len(pretender_str)
+
+		add_zeros := (4 - pretender_str_len)
+		zeros := strings.Repeat("0", add_zeros)
+
+		joined_str := []string{zeros, pretender_str}
+
+		str[i] = strings.Join(joined_str, "")
+	}
+	return strings.Join(str, ":")
+}
+
+func DispPort(portOcts []byte) string {
+	return strconv.FormatInt(int64(portOcts[0])*256+int64(portOcts[1]), 10)
 }
